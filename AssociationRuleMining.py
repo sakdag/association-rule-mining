da51a5e84cef572c9e7c682974dfcc81d9e44196
Serhat Hakki Akdag <serhatakdag96@gmail.com>
2021-03-27 23:24:25 +0300

    generate_next_set_of_candidates method is added, which creates k+1 tuple itemsets if pair of k item itemsets have their first k-1 item same. Unfinished.

--- AssociationRuleMining.py
+++ AssociationRuleMining.py
@@ -2,12 +2,32 @@ import string
 import random
 
 
-def create_itemset(num_of_itemsets, num_of_item_per_itemset):
+def create_itemset(num_of_itemsets: int, num_of_item_per_itemset: int):
     itemsets = set()
     while len(itemsets) != num_of_itemsets:
-        current_list = list()
-        for i in range(num_of_item_per_itemset):
-            current_list.append(random.choice(string.ascii_lowercase))
-        sorted_list = sorted(current_list)
-        itemsets.add(tuple(sorted_list))
+        current_set = set()
+        while len(current_set) != num_of_item_per_itemset:
+            current_set.add(random.choice(string.ascii_lowercase))
+        sorted_set = sorted(current_set)
+        itemsets.add(tuple(sorted_set))
     return itemsets
+
+
+# k is used to represent number of elements in each itemset in itemsets
+def generate_next_set_of_candidates(itemsets: set[tuple], k: int):
+    next_candidates = set()
+    itemsets_as_list = list(itemsets)
+    is_candidate: bool
+    for i in range(len(itemsets_as_list)):
+        for j in range(i + 1, len(itemsets_as_list)):
+            is_candidate = True
+            for m in range(k - 1):
+                if itemsets_as_list[i][m] != itemsets_as_list[j][m]:
+                    is_candidate = False
+                    break
+            if is_candidate:
+                itemset_to_add = list(itemsets_as_list[i])
+                itemset_to_add.append(list(itemsets_as_list[j])[k - 1])
+                sorted_set_to_add = sorted(itemset_to_add)
+                next_candidates.add(tuple(sorted_set_to_add))
+    return next_candidates
--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import AssociationRuleMining as arm
 
 if __name__ == "__main__":
     itemsets = arm.create_itemset(50, 2)
-    print(list)
+    print(itemsets)
+
+    next_itemsets = arm.generate_next_set_of_candidates(itemsets, 2)
+    print(next_itemsets)
