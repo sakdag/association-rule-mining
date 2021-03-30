57ab2f724d929d750b24a5550a8bfbae8939f102
Serhat Hakki Akdag <serhatakdag96@gmail.com>
2021-03-30 23:02:31 +0300

    Naive itemset generation added according to HW description, completed. Second part (SON algorithm) restructured, will be finished later

--- src/AssociationRuleMining.py
+++ src/AssociationRuleMining.py
@@ -1,6 +1,9 @@
 import itertools
 import string
 import random
+import csv
+import pandas as pd
+from mlxtend.frequent_patterns import apriori
 
 
 def create_itemset(num_of_itemsets: int, num_of_item_per_itemset: int):
@@ -15,8 +18,8 @@ def create_itemset(num_of_itemsets: int, num_of_item_per_itemset: int):
 
 
 # k is used to represent number of elements in each itemset in itemsets
-def generate_next_set_of_candidates(itemsets: set[tuple], k: int):
-    next_candidates = set()
+def generate_next_set_of_itemsets(itemsets: set[tuple], k: int):
+    next_itemsets = set()
     itemsets_as_list = list(itemsets)
     is_candidate: bool
     for i in range(len(itemsets_as_list)):
@@ -30,20 +33,70 @@ def generate_next_set_of_candidates(itemsets: set[tuple], k: int):
                 itemset_to_add = list(itemsets_as_list[i])
                 itemset_to_add.append(list(itemsets_as_list[j])[k - 1])
                 sorted_set_to_add = sorted(itemset_to_add)
-                next_candidates.add(tuple(sorted_set_to_add))
-    return next_candidates
+                next_itemsets.add(tuple(sorted_set_to_add))
 
-
-# k is used to represent number of elements in each itemset in original_itemsets
-# Note that itemsets have k+1 number of items in each element
-def eliminate_itemsets_with_non_frequent_items(original_itemsets: set[tuple], itemsets: set[tuple], k: int):
-    next_candidates = set()
-    for itemset in itemsets:
+    # Eliminate k+1 item itemsets which include non-frequent k item itemsets
+    pruned_itemsets = set()
+    for itemset in next_itemsets:
         is_infrequent = False
         for combination in itertools.combinations(itemset, k):
-            if not(combination in original_itemsets):
+            if not (combination in itemsets):
                 is_infrequent = True
                 break
         if not is_infrequent:
-            next_candidates.add(itemset)
-    return next_candidates
+            pruned_itemsets.add(itemset)
+    return pruned_itemsets
+
+
+# k is used to represent number of elements in each itemset in itemsets
+def generate_next_set_of_naive_itemsets(itemsets: set[tuple], k: int):
+    next_itemsets = set()
+    all_possible_characters = set()
+    for itemset in itemsets:
+        for element in itemset:
+            all_possible_characters.add(element)
+    for itemset in itemsets:
+        current_itemset = list(itemset)
+        for character in all_possible_characters:
+            if character not in current_itemset:
+                next_itemset = list(current_itemset)
+                next_itemset.append(character)
+                sorted_itemset = sorted(next_itemset)
+                next_itemsets.add(tuple(sorted_itemset))
+    return next_itemsets
+
+
+def prepare_dataset(dataset_path: string, te):
+    dataset = list()
+    with open(dataset_path, "r") as f:
+        reader = csv.reader(f, delimiter="\n")
+        for line in reader:
+            current_line = list()
+            for token in str(line[0]).split(";"):
+                current_line.append(token)
+            dataset.append(current_line)
+
+    te_ary = te.fit(dataset).transform(dataset)
+    return dataset, pd.DataFrame(te_ary, columns=te.columns_)
+
+
+def calculate_apriori_with_mlxtend(dataset, dataframe, te):
+    print(len(apriori(dataframe, min_support=0.02, use_colnames=True)))
+
+    # Divide initial dataset into 5 to 10 chunks, run apriori and compare results
+    for i in range(5, 10):
+        print("For partitions of: ", i)
+        partitioned_datasets = list()
+        number_of_elements = int(len(dataset) / i)
+        number_of_elements_in_last = number_of_elements + (len(dataset) - (i * number_of_elements))
+        for j in range(i):
+            current_dataset = list()
+            for k in range(j * number_of_elements, (j + 1) * number_of_elements):
+                current_dataset.append(dataset[k])
+            partitioned_datasets.append(current_dataset)
+        for j in range(number_of_elements, number_of_elements_in_last):
+            partitioned_datasets[i - 1].append(dataset[(number_of_elements * (i - 1)) + j])
+        for j in range(i):
+            part_te_ary = te.fit(partitioned_datasets[j]).transform(partitioned_datasets[j])
+            part_df = pd.DataFrame(part_te_ary, columns=te.columns_)
+            print(len(apriori(part_df, min_support=0.02, use_colnames=True)))
--- src/main.py
+++ src/main.py
@@ -1,56 +1,30 @@
-import csv
-
-import AssociationRuleMining as arm
-import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
-from mlxtend.frequent_patterns import apriori
+import AssociationRuleMining as arm
 
 
 if __name__ == "__main__":
-    itemsets = arm.create_itemset(50, 2)
+
+    # Question 1 / Implementing part of apriori algorithm
+    itemsets = arm.create_itemset(50, 3)
     #print(itemsets)
 
-    extended_itemsets = arm.generate_next_set_of_candidates(itemsets, 2)
-    #print(extended_itemsets)
-
-    pruned_itemsets = arm.eliminate_itemsets_with_non_frequent_items(itemsets, extended_itemsets, 2)
+    pruned_itemsets = arm.generate_next_set_of_itemsets(itemsets, 3)
     #print(pruned_itemsets)
 
-    print("Number of elements in extended itemsets: ", len(extended_itemsets))
-    print("Number of elements in extended itemsets after pruning: ", len(pruned_itemsets))
+    naive_itemsets = arm.generate_next_set_of_naive_itemsets(itemsets, 3)
+    #print(naive_itemsets)
 
-    reduction = float(len(pruned_itemsets)) / float(len(extended_itemsets))
+    print("Number of elements in naive itemsets: ", len(naive_itemsets))
+    print("Number of elements in itemsets after pruning: ", len(pruned_itemsets))
+
+    reduction = float(len(pruned_itemsets)) / float(len(naive_itemsets))
     print("Calculated reduction after pruning is: ", reduction)
+    # End of Question 1
 
-    dataset = list()
-    with open("../resources/groceries.csv", "r") as f:
-        reader = csv.reader(f, delimiter="\n")
-        for line in reader:
-            current_line = list()
-            for token in str(line[0]).split(";"):
-                current_line.append(token)
-            dataset.append(current_line)
-
+    # Question 2 / Experimenting with SON algorithm
     te = TransactionEncoder()
-    te_ary = te.fit(dataset).transform(dataset)
-    df = pd.DataFrame(te_ary, columns=te.columns_)
-    # print(len(df))
+    dataset, dataframe = arm.prepare_dataset("../resources/groceries.csv", te)
+    #print(len(dataframe))
 
-    print(len(apriori(df, min_support=0.02, use_colnames=True)))
-
-    for i in range(5, 10):
-        print("For partitions of: ", i)
-        partitioned_datasets = list()
-        number_of_elements = int(len(dataset) / i)
-        number_of_elements_in_last = number_of_elements + (len(dataset) - (i * number_of_elements))
-        for j in range(i):
-            current_dataset = list()
-            for k in range((j) * number_of_elements, (j + 1) * number_of_elements):
-                current_dataset.append(dataset[k])
-            partitioned_datasets.append(current_dataset)
-        for j in range(number_of_elements, number_of_elements_in_last):
-            partitioned_datasets[i - 1].append(dataset[(number_of_elements * (i - 1)) + j])
-        for j in range(i):
-            part_te_ary = te.fit(partitioned_datasets[j]).transform(partitioned_datasets[j])
-            part_df = pd.DataFrame(part_te_ary, columns=te.columns_)
-            print(len(apriori(part_df, min_support=0.02, use_colnames=True)))
+    arm.calculate_apriori_with_mlxtend(dataset, dataframe, te)
+    # End of Question 2
