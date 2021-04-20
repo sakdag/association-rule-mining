89b82d06862028f9980ece33d9f4a4024842aa32
sakdag <serhatakdag96@gmail.com>
2021-04-20 10:31:19 +0300

    Final corrections are made due to changes in homework descriptions.

--- src/main.py
+++ src/main.py
@@ -6,20 +6,29 @@ if __name__ == "__main__":
     # Question 1 / Implementing part of apriori algorithm
     print("-------------------------- Question 1 --------------------------")
 
-    itemsets = arm.create_itemset(50, 2)
-    #print(itemsets)
+    number_of_itemsets = 55
+    current_k = 2
 
-    pruned_itemsets = arm.generate_next_set_of_itemsets(itemsets, 2)
-    #print(pruned_itemsets)
+    for i in range(50):
+        if 20 <= i < 40:
+            current_k = 3
+        elif i > 40:
+            current_k = 4
 
-    naive_itemsets = arm.generate_next_set_of_naive_itemsets(itemsets, 2)
-    #print(naive_itemsets)
+        itemsets = arm.create_itemsets(number_of_itemsets, current_k)
+        # print(itemsets)
 
-    print("Number of elements in naive itemsets: ", len(naive_itemsets))
-    print("Number of elements in itemsets after pruning: ", len(pruned_itemsets))
+        pruned_itemsets = arm.generate_next_set_of_itemsets(itemsets, current_k)
+        # print(pruned_itemsets)
 
-    reduction = float(len(pruned_itemsets)) / float(len(naive_itemsets))
-    print("Calculated reduction after pruning is: ", reduction)
+        naive_itemsets = arm.generate_next_set_of_naive_itemsets(itemsets, current_k)
+        # print(naive_itemsets)
+
+        print("Number of elements in list of itemsets for candidate generation algorithm: ", len(pruned_itemsets))
+        print("Number of elements in list of naive itemsets: ", len(naive_itemsets))
+
+        reduction = float(len(pruned_itemsets)) / float(len(naive_itemsets))
+        print("Calculated reduction in candidate generation is: ", reduction, "\n")
     # End of Question 1
 
     # Question 2 / Experimenting with SON algorithm
@@ -27,7 +36,7 @@ if __name__ == "__main__":
 
     te = TransactionEncoder()
     dataset, dataframe = arm.prepare_dataset("../resources/groceries.csv", te)
-    #print(len(dataframe))
+    # print(len(dataframe))
 
     arm.calculate_apriori_with_mlxtend(dataset, dataframe, te)
     # End of Question 2
